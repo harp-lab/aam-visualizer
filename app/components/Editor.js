57c02dad3b14d833e28f569ccf00137e6d6d3b34
cry3rn <cry3rn@gmail.com>
2019-05-23 13:13:21 -0500

    fixed editor mark selection bug

--- app/components/Editor.js
+++ app/components/Editor.js
@@ -28,7 +28,7 @@ class Editor extends Component {
       element.classList.add('mark');
       element.textContent = id;
       element.addEventListener('click', event => {
-        this.props.onSelect(id);
+        this.props.onNodeSelect(id);
       });
       doc.setBookmark(mark.end, { widget: element, insertLeft: true });
     }
