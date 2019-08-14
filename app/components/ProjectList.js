5028a5b1049bb1ac6c7da5ff951b9b1d7f65b293
cry3rn <cry3rn@gmail.com>
2019-08-14 13:09:24 -0500

    added project export button to project view

--- app/components/ProjectList.js
+++ app/components/ProjectList.js
@@ -92,30 +92,6 @@ function ProjectList(props) {
       body: JSON.stringify({ name })
     });
   }
-  async function exportProject(projectId) {
-    const project = projects[projectId];
-    await props.onGet(projectId);
-
-    // get project data
-    const data = project.export();
-    const json = JSON.stringify(data, null, 2);
-    const blob = new Blob([json], { type: 'application/json' });
-
-    // create elem
-    const href = URL.createObjectURL(blob);
-    const file = `aam-vis-${projectId}.json`
-    const elem = document.createElement('a');
-    Object.assign(elem, {
-      href,
-      download: file
-    });
-    document.body.appendChild(elem);
-    elem.click();
-
-    // cleanup
-    elem.remove();
-    URL.revokeObjectURL(href);
-  }
 
   const projectList = Object.entries(projects).map(([id, project]) => {
     const analysisElem = (
@@ -158,7 +134,7 @@ function ProjectList(props) {
         <DropMenu
           items={ [
             {label: 'Rename', callback: () => openRenameDialog(id)},
-            {label: 'Export', callback: () => exportProject(id)}
+            {label: 'Export', callback: () => props.onExport(id)}
           ] } />
         <Tooltip title='Fork'>
           <IconButton onClick={ () => props.onFork(id) }>
