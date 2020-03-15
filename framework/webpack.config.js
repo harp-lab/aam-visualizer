88d7e54c0ce368d57b2d5641c659dcd0d9193a35
Clark Ren <cry3rn@gmail.com>
2020-03-15 11:29:07 -0500

    moved webpack.config into fext

--- framework/webpack.config.js
+++ framework/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 
-const rootDir = process.cwd();
+const fconfig = require(path.resolve(process.cwd(), 'framework.config.js'));
 
 const target = process.env.npm_lifecycle_event;
 let config;
@@ -14,6 +14,6 @@ switch (target) {
     break;
 }
 
-const configExtension = path.resolve(rootDir, 'webpack.config.js');
+const configExtension = path.resolve(fconfig.FEXT_DIR, 'webpack.config.js');
 
 module.exports = merge(require('./webpack/config.base.js'), config, require(configExtension));
