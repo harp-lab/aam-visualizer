7bbd9806388310c2bd7f4bd29334cb67f7b70e46
Clark Ren <cry3rn@gmail.com>
2020-04-09 17:50:06 -0500

    started implementation of framework cli, new build, added new dependency

--- framework/webpack.config.js
+++ framework/webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 const merge = require('webpack-merge');
 const fconfig = require(path.resolve(process.cwd(), 'framework.config.js'));
 
-const target = process.env.npm_lifecycle_event;
+const { fenv } = process.env;
 let config;
-switch (target) {
-  case 'dev-client':
+switch (fenv) {
+  case 'development':
     config = require('./webpack/config.dev.js')
     break;
-  case 'build':
+  case 'production':
     config = require('./webpack/config.prod.js')
     break;
 }
